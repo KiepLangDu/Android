0a0a4640153ec68d81bdc6f2f6ef8873b1af20fa
KiepLangDu <bigfox.ad@gmail.com>
2023-08-18 18:00:29 +0700

    Add script run on adb

--- ADB_Clean_log.bash
+++ ADB_Clean_log.bash
@@ -1,20 +1,24 @@
 adb shell "
+###First part
+#Clean replays
+echo \"Clean replays and logs except for the last one \"
 cd /sdcard/Android/data/com.riotgames.league.wildriftvn/files/Replays/4000300699301/882728827094849141/
-rm $(ls -t | grep -v $(ls -t |head -n 1)) 2> /sdcard/log.txt
-if [[ $? -eq  0 ]]
-	then
-		echo "Replays are cleaned"
-	else
-		echo "Your replays are ready cleaned before"
+rm \$(ls -t | grep -v \$(ls -t |head -n 1)) 2> /sdcard/log.txt
+if [[ \$? -eq  0 ]]
+then
+	echo \"Replays are cleaned\"
+else
+	echo \"Your replays are ready cleaned before\"
 fi
+## Next Part
+#Clearn Log files
 cd /sdcard/Android/data/com.riotgames.league.wildriftvn/files/Log/
-rm $(ls -t *.log 2> /sdcard/log.txt | grep -v 2> /sdcard/log.txt $(ls -t *.log 2> /sdcard/log.txt| head -n 1)) 2> /sdcard/log.txt
-if [[ $? -eq 0 ]]
-	then
-		echo "Logs are cleaned"
-	else
-		echo "Your logs are ready cleaned before"
+rm \$(ls -t *.log 2> /sdcard/log.txt | grep -v \$(ls -t *.log 2> /sdcard/log.txt |head -n 1)) 2> /sdcard/log.txt
+if [[ \$? -eq 0 ]]
+then
+	echo \"Logs are cleaned\"
+else
+	echo \"Your logs are ready cleaned before\"
 fi
 rm /sdcard/log.txt
-echo "Done"
 "
