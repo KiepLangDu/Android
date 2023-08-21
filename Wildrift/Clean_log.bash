897a895a45fece28f24f8f796287fe3bb37499f1
KiepLangDu <bigfox.ad@gmail.com>
2023-08-21 14:48:12 +0700

    Update Clean_log

--- Wildrift/Clean_log.bash
+++ Wildrift/Clean_log.bash
@@ -1,8 +1,12 @@
 #### Clean Replays
 # On android 12 and above you need root permission to access /sdcard/Android/data
 #ALWAYS REMEMBER TO CHECK PATH OF DIRECTORIES ON YOUR STORAGE BEFORE RUN THIS CODE
+Start(){
 echo "Clean replays and logs except for the last one \n"
 #cd /sdcard/Android/data/com.riotgames.league.wildriftvn/files/Replays/4000300699301/882728827094849141/
+Replays
+}
+Replays(){
 cd /sdcard/Android/data/com.riotgames.league.wildriftvn/files/Replays/*/*/
 rm $(ls -t | grep -v $(ls -t |head -n 1)) 2> /sdcard/log.txt
 if [[ $? -eq  0 ]]
@@ -11,15 +15,18 @@ if [[ $? -eq  0 ]]
 	else
 		echo "Your replays are ready cleaned before"
 fi
+Logs
+}
+Logs(){
 ### Clean Log
 cd /sdcard/Android/data/com.riotgames.league.wildriftvn/files/Log/
-rm $(ls -t *.log| grep -v $(ls -t *.log | head -n 1)) 2> /sdcard/log.txt
+rm $(ls -t *.log 2> /sdcard/log.txt| grep -v $(ls -t *.log 2>/sdcard/log.txt | head -n 1) 2>/sdcard/log.txt) 2> /sdcard/log.txt
 if [[ $? -eq 0 ]]
 	then
 		echo "Logs are cleaned"
 	else
 		echo "Your logs are ready cleaned before"
 fi
+}
+Start
 rm /sdcard/log.txt
-echo "\nDone!"
-
