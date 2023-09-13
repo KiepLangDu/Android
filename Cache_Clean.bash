b75be16450963258541d95c5ee4fdef797b052c0
KiepLangDu <bigfox.ad@gmail.com>
2023-09-13 18:06:36 +0700

    Update Cache_Clean.bash

--- Cache_Clean.bash
+++ Cache_Clean.bash
@@ -12,30 +12,35 @@ Start(){
 
 	echo " The number of cache directory will be remove is: $limit \nDo you want to remove them (y) or (n)"
 	read flag
-	Clean(){
-		x=1
-		while [ $x -le $limit ]
-		do
-			flag=$(find /data/data/ -name cache |head -n $x | tail -n 1)
-			if [[ $x -eq $quater ]]
+	Case
+}
+Clean(){
+	x=1
+	while [ $x -le $limit ]
+	do
+		flag=$(find /data/data/ -name cache |head -n $x | tail -n 1)
+		if [[ $x -eq $quater ]]
+		then
+			echo "Your progress is 25%\nCurrent flag is: $flag"
+		elif [[ $x -eq $half ]]
+		then
+			echo "Your progress is 50%\nCurrent flag is: $flag"
+		elif [[ $x -eq $three_quater ]]
 			then
-				echo "Your progress is 25%\nCurrent flag is: $flag"
-			elif [[ $x -eq $half ]]
-			then
-				echo "Your progress is 50%\nCurrent flag is: $flag"
-			elif [[ $x -eq $three_quater ]]
-			then
-				echo "Your progress is 75%\nCurrent flag is: $flag"
-			elif [[ $x -eq $limit ]]
-			then
-				echo "Your progress is 100%\nFinal flag is: $flag"
-			fi
-			#echo "Removing:  $flag"
-			rm -rf $flag
-			x=$(($x+1))
-		done
-		echo "Done!"
-	}
+			echo "Your progress is 75%\nCurrent flag is: $flag"
+		elif [[ $x -eq $limit ]]
+		then
+			Showing
+			echo "Your progress is 100%\nFinal flag is: $flag"
+		fi
+		#echo "Removing:  $flag"
+		rm -rf $flag
+		x=$(($x+1))
+	done
+	echo "Done!"
+}
+Case(){
+
 	case $flag in
 		y)
 			echo "You chose to remove all cache"
@@ -52,4 +57,26 @@ Start(){
 			;;
 	esac
 }
+################## Showing progress ###############
+Showing(){
+	x=0
+	limit=20
+	while [ $x -le $limit ]
+	do
+		y=0
+		while [ $y -le $limit ]
+		do
+			if [[ $x -eq 0 || $y -eq 0 || $x -eq $limit || $y -eq $limit ]]
+			then
+				printf "*"
+			else
+				printf " "
+			fi
+			y=$(($y+1))
+		done
+		printf "\n"
+		x=$(($x+1))
+	done
+}
+################# End of Showing progress ##########
 Start
